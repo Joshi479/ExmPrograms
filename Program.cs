904f93fe178ab9d567f5f43f1becedc8da561ee9
Joshi479 <joshisaiteja79@gmail.com>
2017-03-13 18:07:14 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibbonacciProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("enter a number");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int a = 0, x;
+                int b = 1;
+                Console.Write(" ");
+                for (int i = 0; i <= n; i++)
+                {
+                    x = a + b;
+                    a = b;
+                    b = x;
+                    Console.Write("{0} ", x);
+                }
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("enter a valid integer");
+            }
+        }
+    }
+}
---
+++ StarsProg/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarsProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Number of rows to be printed.");
+                int value = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i <= value; i++)
+                {
+                    for (int j = 0; j < value - i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine("\n");
+                }
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Enter a valid positive integer");
+            }
+        }
+    }
+}
